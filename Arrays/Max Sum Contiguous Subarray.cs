637c6a5853a30226f07bae1333a5a374cac88147
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-04 00:09:26 +0530

    Max Sum Contiguous Subarray

---
+++ Arrays/Max Sum Contiguous Subarray.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewBit.Arrays
+{
+    class Max_Sum_Contiguous_Subarray
+    {
+        public int maxSubArray(List<int> A)
+        {
+            int sum = A[0], currentSum = 0;
+            int length = A.Count;
+            for (int i = 0; i < length; i++) 
+            {
+                currentSum += A[i];
+                sum = Math.Max(sum, currentSum);
+                if (currentSum < 0) 
+                {
+                    currentSum = 0;
+                }
+            }
+            return sum;
+        }
+    }
+}
