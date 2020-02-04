0dc70bbafa2aadf5abc0d73afe6b0e777c8994cf
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-04 20:55:28 +0530

    Max Non Negative SubArray

---
+++ Arrays/Max Non Negative SubArray.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewBit.Arrays
+{
+    class Max_Non_Negative_SubArray
+    {
+        public List<int> maxset(List<int> A)
+        {
+            long maxsum = 0, cursum, currentNumberIncludedCount, numberIncludedCount = 0;
+            int i = 0, j = 0, l = 0, r = 0;
+            long size = A.Count;
+            while (i < size)
+            {
+                currentNumberIncludedCount = 0;
+                cursum = 0;
+                while (j < size && A[j] >= 0)
+                {
+                    cursum += A[j];
+                    j++;
+                    currentNumberIncludedCount++;
+                }
+                if (cursum > maxsum)
+                {
+                    l = i;
+                    r = j;
+                    maxsum = cursum;
+                    numberIncludedCount = currentNumberIncludedCount;
+                }
+                else if (cursum == maxsum)
+                {
+                    if (currentNumberIncludedCount > numberIncludedCount)
+                    {
+                        l = i;
+                        r = j;
+                        numberIncludedCount = currentNumberIncludedCount;
+                    }
+                }
+                while (j < size && A[j] < 0)
+                {
+                    j++;
+                }
+                i = j;
+            }
+            List<int> list = new List<int>();
+            for (int k = l; k < r; k++)
+            {
+                list.Add(A[k]);
+            }
+            return list;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace InterviewBit
+﻿using InterviewBit.Arrays;
+using System.Collections.Generic;
+
+namespace InterviewBit
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Max_Non_Negative_SubArray m = new Max_Non_Negative_SubArray();
+            m.maxset(new List<int> { 0, 0, -1, 0 });
         }
     }
 }
