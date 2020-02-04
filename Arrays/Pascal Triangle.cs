ae749def76e91289aa2c01f29a8230dddaf190b1
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-05 00:52:32 +0530

    Pascal Triangle

---
+++ Arrays/Pascal Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewBit.Arrays
+{
+    class Pascal_Triangle
+    {
+        public List<List<int>> solve(int A)
+        {
+            List<List<int>> listOuter = new List<List<int>>();
+            int line = 1;
+            for (int i = 1; i <= A; i++)
+            {
+                List<int> listInner = new List<int>();
+                for(int j = 1; j <= i; j++) 
+                {
+                    if (j == 1 || j == i)
+                    {
+                        listInner.Add(1);
+                    }
+                    else 
+                    {
+                        listInner.Add((listOuter[i - 2])[j-1] + (listOuter[i - 2])[j - 2]);
+                    }
+                }
+                listOuter.Add(listInner);
+            }
+            return listOuter;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,9 +9,12 @@ namespace InterviewBit
         {
             //Max_Non_Negative_SubArray m = new Max_Non_Negative_SubArray();
             //m.maxset(new List<int> { 0, 0, -1, 0 });
-            Spiral_Order_Matrix_II sp = new Spiral_Order_Matrix_II();
-            var a = sp.generateMatrix(80);
-            
+            //Spiral_Order_Matrix_II sp = new Spiral_Order_Matrix_II();
+            //var a = sp.generateMatrix(80);
+            Pascal_Triangle p = new Pascal_Triangle();
+            p.solve(4);
+
+
         }
     }
 }
