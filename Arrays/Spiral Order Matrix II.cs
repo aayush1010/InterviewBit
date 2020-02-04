20061b711a359722a96a2ca9a1ec4036472b13d9
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-05 00:36:58 +0530

    Spiral order matrix

---
+++ Arrays/Spiral Order Matrix II.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewBit.Arrays
+{
+    class Spiral_Order_Matrix_II
+    {
+        public List<List<int>> generateMatrix(int A)
+        {
+            List<List<int>> spiral = new List<List<int>>();
+            int i, k = 0, l = 0, m = A, n = A, count = 1;
+
+            while (k < m && l < n)
+            {
+                for (i = l; i < n; ++i)
+                {
+                    spiral[k][i] = count;
+                    count++;
+                }
+                k++;
+
+                for (i = k; i < m; ++i)
+                {
+                    spiral[i][n - 1] = count;
+                    count++;
+                }
+                n--;
+
+                if (k < m)
+                {
+                    for (i = n - 1; i >= l; --i)
+                    {
+                        spiral[m - 1][i] = count;
+                        count++;
+                    }
+                    m--;
+                }
+                if (l < n)
+                {
+                    for (i = m - 1; i >= k; --i)
+                    {
+                        spiral[i][l] = count;
+                        count++;
+                    }
+                    l++;
+                }
+            }
+            return spiral;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,8 +7,11 @@ namespace InterviewBit
     {
         static void Main(string[] args)
         {
-            Max_Non_Negative_SubArray m = new Max_Non_Negative_SubArray();
-            m.maxset(new List<int> { 0, 0, -1, 0 });
+            //Max_Non_Negative_SubArray m = new Max_Non_Negative_SubArray();
+            //m.maxset(new List<int> { 0, 0, -1, 0 });
+            Spiral_Order_Matrix_II sp = new Spiral_Order_Matrix_II();
+            var a = sp.generateMatrix(80);
+            
         }
     }
 }
