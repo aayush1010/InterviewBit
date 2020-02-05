8d43fa6cad9ee5cf4483d59c7ac1a53ad1b95208
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-05 23:00:56 +0530

    Hotel Bookings Possible

---
+++ Arrays/Hotel Bookings Possible.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewBit.Arrays
+{
+    class Hotel_Bookings_Possible
+    {
+        public int hotel(List<int> arrive, List<int> depart, int K)
+        {
+            arrive.Sort();
+            depart.Sort();
+            int roomRequired = 1, i = 1, j = 0, n = arrive.Count, maxroom = 1;
+
+            while (i < n && j < n)
+            {
+                if (arrive[i] < depart[j])
+                {
+                    i++;
+                    roomRequired++;
+                    if (roomRequired > maxroom)
+                    {
+                        maxroom = roomRequired;
+                    }
+                }
+                else
+                {
+                    j++;
+                    roomRequired--;
+                }
+            }
+
+            return K >= maxroom ? 1 : 0;
+        }
+    }
+}
