1286a3bf4a17dbb313b2649568dfe9b86b156b41
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-05 23:54:59 +0530

    Find Duplicate in Array

---
+++ Arrays/Find Duplicate in Array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewBit.Arrays
+{
+    class Find_Duplicate_in_Array
+    {
+        // mark visited number with negative of itself
+        public int repeatedNumber(List<int> a)
+        {
+            for (int i = 0; i < a.Count; i++)
+            {
+                if (a[Math.Abs(a[i])] > 0)
+                {
+                    a[Math.Abs(a[i])] = -1 * a[Math.Abs(a[i])];
+                }
+                else
+                {
+                    return Math.Abs(a[i]);
+                }
+            }
+            return -1;
+        }
+    }
+}
