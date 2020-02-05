7bfc10fa660ec4e3ba7080d74a739af1bfde9ab2
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-05 22:46:27 +0530

    Noble Integer

---
+++ Arrays/Noble Integer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewBit.Arrays
+{
+    class Noble_Integer
+    {
+        public int solve(List<int> A)
+        {
+            A.Sort();
+            if (A[A.Count - 1] == 0)
+            {
+                return 1;
+            }
+            for (int i = 0; i < A.Count - 1; i++)
+            {
+                if (A[i] == A[i + 1])
+                {
+                    continue;
+                }
+                if (A[i] == A.Count - i - 1)
+                {
+                    return 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
