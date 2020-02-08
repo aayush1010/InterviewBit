2e8123290ffd6a8af448c87422c04d7f28f07f9b
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-08 16:47:43 +0530

    Max Sp Prod

---
+++ Arrays/MaxSpProd.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewBit.Arrays
+{
+    class MaxSpProd
+    {
+        public int maxSpecialProduct(List<int> A)
+        {
+            Stack<int> stack = new Stack<int>();
+            int[] nxtGreaterElement = new int[A.Count];
+            int[] previousGreaterElement = new int[A.Count];
+
+
+            for (int i = A.Count - 1; i >= 0; i--)
+            {
+                while (stack.Count != 0 && stack.Peek() < A[i])
+                {
+                    stack.Pop();
+                }
+                nxtGreaterElement[i] = stack.Count == 0 ? 0 :i;
+                stack.Push(A[i]);
+            }
+
+            stack = new Stack<int>();
+            for (int i = 0; i < A.Count; i++)
+            {
+                while (stack.Count != 0 && stack.Peek() < A[i])
+                {
+                    stack.Pop();
+                }
+                previousGreaterElement[i] = stack.Count == 0 ? 0 : i;
+                stack.Push(A[i]);
+            }
+
+            int max = int.MinValue;
+            for (int i = 0; i < A.Count; i++)
+            {
+                max = Math.Max(max, ((previousGreaterElement[i] % 1000000007) * (nxtGreaterElement[i] % 1000000007)) % 1000000007);
+            }
+
+            return max;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,8 +11,11 @@ namespace InterviewBit
             //m.maxset(new List<int> { 0, 0, -1, 0 });
             //Spiral_Order_Matrix_II sp = new Spiral_Order_Matrix_II();
             //var a = sp.generateMatrix(80);
-            Pascal_Triangle p = new Pascal_Triangle();
-            p.solve(4);
+            //Pascal_Triangle p = new Pascal_Triangle();
+            //p.solve(4);
+
+            MaxSpProd mp = new MaxSpProd();
+            mp.maxSpecialProduct(new List<int> { 5, 9, 6, 8, 6, 4, 6, 9, 5, 4, 9 });
 
 
         }
