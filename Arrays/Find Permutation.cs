27d0a68f8d9f5174e8cfb13a371077c09b02baa5
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-09 01:24:21 +0530

    Find Permutation

---
+++ Arrays/Find Permutation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewBit.Arrays
+{
+    class Find_Permutation
+    {
+        List<int> findPerm(string A, int B)
+        {
+            List<int> v = new List<int>();
+            for (int i = 0; i < B; i++)
+            {
+                v.Add(i + 1);
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 'D')
+                {
+                    int d = 0;
+                    int j = i;
+                    while (j < A.Length && A[j] == 'D')
+                    {
+                        d++;
+                        j++;
+                    }
+                    v.Reverse(i, d + 1);
+                    i = j;
+                }
+            }
+            return v;
+        }
+    }
+}
