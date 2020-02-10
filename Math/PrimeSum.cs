0e74c87a1e517a1a647b7227d53e9bb7ba5a57d8
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-11 00:24:12 +0530

    Prime Sum

---
+++ Math/PrimeSum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewBit.Math
+{
+    class PrimeSum
+    {
+        public List<int> primesum(int A)
+        {
+            bool[] lisOfPrimesLessThanA = GetPrimes(A);
+
+            List<int> list = new List<int>();
+            for (int i = 2; i < A; i++)
+            {
+                if (lisOfPrimesLessThanA[i] && lisOfPrimesLessThanA[A - i])
+                {
+                    list.Add(i);
+                    list.Add(A - i);
+                    break;
+                }
+            }
+            return list;
+        }
+
+        private bool[] GetPrimes(int a)
+        {
+            bool[] isPrime = new bool[a + 1];
+            for (int i = 2; i <= a; i++)
+            {
+                isPrime[i] = true;
+            }
+            for (int i = 2; i * i <= a; i++)
+            {
+                if (isPrime[i])
+                {
+                    for (int j = i * i; j <= a; j += i)
+                    {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+
+            return isPrime;
+        }
+    }
+}
