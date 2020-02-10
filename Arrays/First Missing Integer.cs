da0fd182c1ea93361e431bb1ff2089726e0b9cfb
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-11 00:02:08 +0530

    Next missing integer

---
+++ Arrays/First Missing Integer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewBit.Arrays
+{
+    class First_Missing_Integer
+    {
+        public int firstMissingPositive(List<int> A)
+        {
+            int count = A.Count;
+            bool[] present = new bool[count+1];
+            for (int i = 0; i < count; i++) 
+            {
+                if (A[i] > 0 && A[i] <= count) 
+                {
+                    present[A[i]] = true;
+                }
+            }
+
+            for (int i = 1; i < present.Length; i++) 
+            {
+                if (!present[i]) 
+                {
+                    return i;
+                }
+            }
+
+            return count + 1;
+        }
+    }
+}
