65faaabbe4c459e4d8a4808e19f7fd43687aa719
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-12 00:18:07 +0530

    Power of two integers

---
+++ Math/Power Of Two Integers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InterviewBit.Math
+{
+    class Power_Of_Two_Integers
+    {
+        public int isPower(int A)
+        {
+            if (A == 1)
+            {
+                return 1;
+            }
+            for (int i = 2; i * i <= A; i++)
+            {
+                double y = Math.Log(A) / Math.Log(i);
+                if ((y - (int)y) < 0.00000001)
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+    }
+}
