5beba1c5f2d471a0a9db741df7d27309a52805e2
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-12 00:43:37 +0530

    Excel column number

---
+++ Math/Excel_Column_Number.cs
@@ -0,0 +1,16 @@
+﻿namespace InterviewBit.Math
+{
+    class Excel_Column_Number
+    {
+        public int titleToNumber(string A)
+        {
+            int sum = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                sum *= 26;
+                sum += (A[i] - 64);
+            }
+            return sum;
+        }
+    }
+}
