40cc57522eb8f4331aa2b1d9604774260082e92c
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-11 23:56:10 +0530

    FizzBuzz

---
+++ Math/FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace InterviewBit.Math
+{
+    class FizzBuzz
+    {
+        public List<string> fizzBuzz(int A)
+        {
+            List<string> res = new List<string>();
+            for (int i = 1; i <= A; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    res.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    res.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    res.Add("Buzz");
+                }
+                else
+                {
+                    res.Add(i.ToString());
+                }
+            }
+            return res;
+        }
+    }
+}
