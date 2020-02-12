f492ce5b45229511743d72ea69397f6a21eebeb5
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-12 23:15:54 +0530

    Excel column title

---
+++ Math/Excel Column Title.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewBit.Math
+{
+    class Excel_Column_Title
+    {
+        public string convertToTitle(int A)
+        {
+            string s = string.Empty;
+            while (A > 0)
+            {
+                if (A % 26 == 0)
+                {
+                    s += "Z";
+                    A = A / 26 - 1;
+                }
+                else
+                {
+                    s += (char)(((A % 26) - 1) + 'A');
+                    A /= 26;
+                }
+            }
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
