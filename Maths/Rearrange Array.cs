895ebda05a4f10293bedd26752d9f97a68aa1c9c
Aayush Mittal <aayush.mittal@pinelabs.com>
2020-02-14 00:32:25 +0530

    rearrange array

---
+++ Maths/Rearrange Array.cs
@@ -0,0 +1,21 @@
+﻿namespace InterviewBit.Maths
+{
+    class Rearrange_Array
+    {
+        // First step: Increase all values  
+        // by (arr[arr[i]] % n) * n 
+        // Second Step: Divide all values by n 
+        void rearrange(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] += (arr[arr[i]] % n) * n;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] /= n;
+            }
+        }
+    }
+}
