6ff9d4a0545d0ecdc9841eed109831c251d737de
aayush1010 <aayushmittal1999@gmail.com>
2020-02-24 23:51:05 +0530

    Longest Common Prefix

---
+++ Strings/Longest Common Prefix.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace InterviewBit.Strings
+{
+    class Longest_Common_Prefix
+    {
+        public string longestCommonPrefix(List<string> A)
+        {
+            int i = 0, k = 0;
+            int minLength = A.Min(y => y.Length);
+            string shortest = A.FirstOrDefault(x => x.Length == minLength);
+            string s = string.Empty;
+            while (i < shortest.Length)
+            {
+                var temp = A[0][i];
+                for (int j = 1; j < A.Count; j++)
+                {
+                    if (A[j][i] != temp)
+                    {
+                        return s;
+                    }
+                }
+                s += temp;
+                i++;
+            }
+            return s;
+        }
+    }
+}
