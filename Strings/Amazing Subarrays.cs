4509b150cdb0921d69b284d00412eaff3f5826c0
aayush1010 <aayushmittal1999@gmail.com>
2020-02-25 00:35:20 +0530

    Amazing Subarrays

---
+++ Strings/Amazing Subarrays.cs
@@ -0,0 +1,19 @@
+﻿namespace InterviewBit.Strings
+{
+    class Amazing_Subarrays
+    {
+        public int solve(string A)
+        {
+            int c = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 'A' || A[i] == 'E' || A[i] == 'I' || A[i] == 'O' || A[i] == 'U'
+                || A[i] == 'a' || A[i] == 'e' || A[i] == 'i' || A[i] == 'o' || A[i] == 'u')
+                {
+                    c += (A.Length - i);
+                }
+            }
+            return c % 10003;
+        }
+    }
+}
