edf3c36b11e4c2cf28652e44c21749eb2e12a8d9
aayush1010 <aayushmittal1999@gmail.com>
2020-03-04 23:20:27 +0530

    Implement StrStr

---
+++ Strings/Implement StrStr.cs
@@ -0,0 +1,32 @@
+﻿namespace InterviewBit.Strings
+{
+    class Implement_StrStr
+    {
+        public int strStr(string A, string B)
+        {
+            int n = A.Length;
+            int z = B.Length;
+            if (n == 0 || z == 0)
+            {
+                return -1;
+            }
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                if (A[i] == B[0])
+                {
+                    j = 0;
+                    while (j < z && j + i < n && A[j + i] == B[j])
+                    {
+                        j++; 
+                    }
+                    if (j == z)
+                    {
+                        return i;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
